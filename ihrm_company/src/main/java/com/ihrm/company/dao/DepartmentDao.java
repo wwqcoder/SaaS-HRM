a94cee6bdb17e8359cca4d6e4147b7d9fe782cd3
wangweiqi <799819401@qq.com>
2019-09-22 20:50:24 +0800

    部门

---
+++ ihrm_company/src/main/java/com/ihrm/company/dao/DepartmentDao.java
@@ -0,0 +1,11 @@
+package com.ihrm.company.dao;
+
+import com.ihrm.domain.company.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ *  部门操作持久化
+ */
+public interface DepartmentDao extends JpaRepository<Department,String> , JpaSpecificationExecutor<Department> {
+}
